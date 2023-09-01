a0440576e483cad70121056fa757a2f698ba1f36
domager73 <112970685+domager73@users.noreply.github.com>
2023-09-01 15:17:36 +0300

    ckfdbr [etcjc

--- lib/pages/home/account_page.dart
+++ lib/pages/home/account_page.dart
@@ -107,52 +107,47 @@ class _AccountPageState extends State<AccountPage> {
                         child: SizedBox(
                           width: width * 0.86,
                           height: 144 / 340 * width * 0.86,
-                          child: Stack(
-                            children: [
-                              SvgPicture.asset(
-                                'assets/pages/account/border.svg',
-                                width: width * 0.86,
-                              ),
-                              Center(
-                                child: Column(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    Text(
-                                      temp['email'] ?? '',
-                                      style: AppTypography.spaseMono16.copyWith(fontWeight: FontWeight.w700)
+                          child: CustomPaint(
+                            painter: AccountBorderPainter(),
+                            child: Center(
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Text(
+                                    temp['email'] ?? '',
+                                    style: AppTypography.spaseMono16.copyWith(fontWeight: FontWeight.w700)
+                                  ),
+                                  Padding(
+                                    padding:
+                                        EdgeInsets.only(top: 2 / 96 * height),
+                                    child: PresButton(
+                                      player: GetIt.I<MusicManager>()
+                                          .keyBackSignCloseX,
+                                      onTap: () {
+                                        GetIt.I<MusicManager>()
+                                            .popupSubmenuPlayer
+                                            .play()
+                                            .then((value) => GetIt.I<
+                                                    MusicManager>()
+                                                .popupSubmenuPlayer
+                                                .seek(Duration(seconds: 0)));
+                                        AccountPageComponents
+                                            .openChangeEmailModalPage(
+                                          context: context,
+                                          width: width,
+                                          height: height,
+                                        );
+                                      },
+                                      params: {
+                                        'width': width,
+                                        'height': height
+                                      },
+                                      child: changeEmailBtn,
                                     ),
-                                    Padding(
-                                      padding:
-                                          EdgeInsets.only(top: 2 / 96 * height),
-                                      child: PresButton(
-                                        player: GetIt.I<MusicManager>()
-                                            .keyBackSignCloseX,
-                                        onTap: () {
-                                          GetIt.I<MusicManager>()
-                                              .popupSubmenuPlayer
-                                              .play()
-                                              .then((value) => GetIt.I<
-                                                      MusicManager>()
-                                                  .popupSubmenuPlayer
-                                                  .seek(Duration(seconds: 0)));
-                                          AccountPageComponents
-                                              .openChangeEmailModalPage(
-                                            context: context,
-                                            width: width,
-                                            height: height,
-                                          );
-                                        },
-                                        params: {
-                                          'width': width,
-                                          'height': height
-                                        },
-                                        child: changeEmailBtn,
-                                      ),
-                                    ),
-                                  ],
-                                ),
+                                  ),
+                                ],
                               ),
-                            ],
+                            ),
                           ),
                         ),
                       ),
@@ -406,5 +401,49 @@ class _AccountPageState extends State<AccountPage> {
         ),
       ),
     );
+  }
+}
+
+class AccountBorderPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    Path path_0 = Path();
+    path_0.moveTo(size.width*0.002924006,size.height*0.2630035);
+    path_0.lineTo(size.width*0.002923977,size.height*0.9962028);
+    path_0.lineTo(size.width*0.2947193,size.height*0.9962028);
+    path_0.lineTo(size.width*0.3144444,size.height*0.9417552);
+    path_0.lineTo(size.width*0.9501550,size.height*0.9417552);
+    path_0.lineTo(size.width*0.9970760,size.height*0.8002028);
+    path_0.lineTo(size.width*0.9970760,size.height*0.008923916);
+    path_0.lineTo(size.width*0.6964854,size.height*0.008923916);
+    path_0.lineTo(size.width*0.6803538,size.height*0.05248035);
+    path_0.lineTo(size.width*0.07477281,size.height*0.05248035);
+    path_0.lineTo(size.width*0.002924006,size.height*0.2630035);
+    path_0.close();
+
+    Paint paint_0_stroke = Paint()..style=PaintingStyle.stroke..strokeWidth=2;
+    paint_0_stroke.color=Colors.white.withOpacity(1.0);
+    canvas.drawPath(path_0,paint_0_stroke);
+
+    Path path_1 = Path();
+    path_1.moveTo(size.width*0.5438596,size.height*0.008924056);
+    path_1.lineTo(size.width*0.07309942,size.height*0.008923916);
+
+    Paint paint_1_stroke = Paint()..style=PaintingStyle.stroke..strokeWidth=2;
+    paint_1_stroke.color=Colors.white.withOpacity(1.0);
+    canvas.drawPath(path_1,paint_1_stroke);
+
+    Path path_2 = Path();
+    path_2.moveTo(size.width*0.9502924,size.height*0.9889441);
+    path_2.lineTo(size.width*0.4415205,size.height*0.9889441);
+
+    Paint paint_2_stroke = Paint()..style=PaintingStyle.stroke..strokeWidth=2;
+    paint_2_stroke.color=Colors.white.withOpacity(1.0);
+    canvas.drawPath(path_2,paint_2_stroke);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
