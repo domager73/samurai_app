44be7238a8108ab7c9daffc9e127b2af34181dd3
domager73 <domager73@gmail.com>
2024-01-16 21:51:40 +0300

    фывафыв

--- lib/pages/home/home_main_page.dart
+++ lib/pages/home/home_main_page.dart
@@ -1,7 +1,5 @@
 import 'dart:async';
 
-import 'package:dio/dio.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_xlider/flutter_xlider.dart';
